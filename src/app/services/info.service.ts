ab4c9a3faac632f95f5af684b8b59371ddf71027
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2020-04-15 01:43:21 +0500

    Converted skills into object

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -1,132 +1,136 @@
 import { Injectable } from '@angular/core';
 import { Skill, Project } from '../utils/classes';
+import { Skills } from '../utils/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoService {
-  skills: Array<Skill>;
+  skills: Skills;
   projects: Array<Project>;
   constructor() {
-    this.skills = [];
     this.projects = [];
     this.initSkills();
     this.initProjects();
   }
 
   initSkills() {
-    this.skills.push(
-      new Skill('Industry', 'Web Development'),
-      new Skill('Industry', 'Hybrid App Development'),
-      new Skill('Industry', 'Native IOS Development'),
-      new Skill('Industry', 'Front End Development'),
-      new Skill('Industry', 'Back End Development'),
-      new Skill('Industry', 'Cloud Computing'),
-      new Skill('Industry', 'Artificial Intelligence'),
-      new Skill('Industry', 'Machine Learning'),
-      new Skill(
+    this.skills = {
+      webDevelopment: new Skill('Industry', 'Web Development'),
+      hybridDevelopment: new Skill('Industry', 'Hybrid App Development'),
+      nativeIosDevelopment: new Skill('Industry', 'Native IOS Development'),
+      frontEndDevelopment: new Skill('Industry', 'Front End Development'),
+      backEndDevelopment: new Skill('Industry', 'Back End Development'),
+      cloudComputing: new Skill('Industry', 'Cloud Computing'),
+      artificialIntelligence: new Skill('Industry', 'Artificial Intelligence'),
+      machineLearning: new Skill('Industry', 'Machine Learning'),
+      git: new Skill(
         'Tool',
         'Git',
         '../../assets/icon/git.png',
         'https://git-scm.com/'
       ),
-      new Skill(
+      github: new Skill(
         'Tool',
         'Github',
         '../../assets/icon/github.png',
         'https://github.com/'
       ),
-      new Skill(
+      firebase: new Skill(
         'Tool',
         'Firebase',
         '../../assets/icon/firebase.png',
         'https://firebase.google.com/'
       ),
-      new Skill(
+      webrtc: new Skill(
         'Tool',
         'WebRTC',
         '../../assets/icon/webrtc.png',
         'https://webrtc.org/'
       ),
-      new Skill('Language', 'HTML 5', '../../assets/icon/html5.png'),
-      new Skill('Language', 'CSS 3', '../../assets/icon/css3.png'),
-      new Skill(
+      html: new Skill('Language', 'HTML 5', '../../assets/icon/html5.png'),
+      css: new Skill('Language', 'CSS 3', '../../assets/icon/css3.png'),
+      sass: new Skill(
         'Language',
         'SASS',
         '../../assets/icon/sass.png',
         'https://sass-lang.com/'
       ),
-      new Skill('Language', 'JavaScript', '../../assets/icon/js.png'),
-      new Skill('Language', 'TypeScript', '../../assets/icon/ts.png'),
-      new Skill(
+      js: new Skill('Language', 'JavaScript', '../../assets/icon/js.png'),
+      ts: new Skill('Language', 'TypeScript', '../../assets/icon/ts.png'),
+      swift: new Skill(
         'Language',
         'Swift',
         '../../assets/icon/swift.png',
         'https://developer.apple.com/swift/'
       ),
-      new Skill('Language', 'C++', '../../assets/icon/c++.png'),
-      new Skill(
+      cpp: new Skill('Language', 'C++', '../../assets/icon/c++.png'),
+      java: new Skill(
         'Language',
         'Java',
         '../../assets/icon/java.png',
         'https://www.java.com/'
       ),
-      new Skill(
+      python: new Skill(
         'Language',
         'Python',
         '../../assets/icon/python.png',
         'https://www.python.org/'
       ),
-      new Skill(
+      angular: new Skill(
         'Framework',
         'Angular',
         '../../assets/icon/angular.png',
         'https://angular.io/'
       ),
-      new Skill(
+      ionic: new Skill(
         'Framework',
         'Ionic',
         '../../assets/icon/ionic.png',
         'https://ionicframework.com/'
       ),
-      new Skill(
+      cordova: new Skill(
         'Framework',
         'Cordova',
         '../../assets/icon/cordova.png',
         'https://cordova.apache.org/'
       ),
-      new Skill(
+      electron: new Skill(
         'Framework',
         'Electron',
         '../../assets/icon/electron.png',
         'https://www.electronjs.org/'
       ),
-      new Skill(
+      capacitor: new Skill(
         'Framework',
         'Capacitor',
         '../../assets/icon/capcitor.png',
         'https://capacitor.ionicframework.com/'
       ),
-      new Skill(
+      swiftui: new Skill(
         'Framework',
         'Swift UI',
         '../../assets/icon/swiftui.png',
         'https://developer.apple.com/xcode/swiftui/'
       ),
-      new Skill(
+      qt: new Skill(
         'Framework',
         'QT',
         '../../assets/icon/qt.png',
         'https://www.qt.io/'
       )
-    );
-    for (const skill of this.skills) {
+    };
+    for (const skill of Object.keys(this.skills)) {
       // Info urls
-      if (!skill.infoUrl) {
-        skill.infoUrl = `https://www.google.com/search?q=${skill.text}`;
+      if (!this.skills[skill].infoUrl) {
+        this.skills[
+          skill
+        ].infoUrl = `https://www.google.com/search?q=${this.skills[skill].text}`;
       }
-      if (!skill.pictureUrl) {
-        skill.pictureUrl = `https://ui-avatars.com/api/?name=${skill.text}`;
+      if (!this.skills[skill].pictureUrl) {
+        this.skills[
+          skill
+        ].pictureUrl = `https://ui-avatars.com/api/?name=${this.skills[skill].text}`;
       }
     }
   }
---
+++ src/app/utils/interfaces.ts
@@ -0,0 +1,32 @@
+import { Skill } from './classes';
+
+export interface Skills {
+  webDevelopment: Skill;
+  hybridDevelopment: Skill;
+  nativeIosDevelopment: Skill;
+  frontEndDevelopment: Skill;
+  backEndDevelopment: Skill;
+  cloudComputing: Skill;
+  artificialIntelligence: Skill;
+  machineLearning: Skill;
+  git: Skill;
+  github: Skill;
+  firebase: Skill;
+  webrtc: Skill;
+  html: Skill;
+  css: Skill;
+  sass: Skill;
+  js: Skill;
+  ts: Skill;
+  swift: Skill;
+  cpp: Skill;
+  java: Skill;
+  python: Skill;
+  angular: Skill;
+  ionic: Skill;
+  cordova: Skill;
+  electron: Skill;
+  capacitor: Skill;
+  swiftui: Skill;
+  qt: Skill;
+}
