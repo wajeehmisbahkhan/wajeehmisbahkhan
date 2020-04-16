5b1b02204e0ecc151d616a8522eef4415e1e8ff9
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2020-04-16 16:26:47 +0500

    Added skill ordering

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { InfoService } from '../services/info.service';
+import { Skill } from '../utils/classes';
+import { SkillTitle } from '../utils/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
-
-  constructor() {}
-
+  skillTitles: Array<SkillTitle>;
+  skills: Array<Skill>;
+  constructor(private info: InfoService) {
+    this.skills = [];
+    const unorderedSkills = Object.values(this.info.skills);
+    // Arrangement
+    this.skillTitles = ['Industry', 'Language', 'Framework', 'Tool'];
+    this.skills = unorderedSkills.sort((a, b) =>
+      this.skillTitles.indexOf(a.title) > this.skillTitles.indexOf(b.title)
+        ? 1
+        : -1
+    );
+  }
 }
